741c56e77e1c6b05bb03925fd722c89e5f05f5c5
ktamas8 <thomas.kapolnasi@gmail.com>
2019-10-17 15:20:38 +0200

    my python module test

--- csv-tutorial.py
+++ csv-tutorial.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import csv
+import url-request
 
 df = pd.read_csv("hrdata.csv")
 #df[['Name','Salary']].head()
@@ -7,7 +8,22 @@ print(df['Name'][0])
 
 with open('hrdata.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
+
     for row in readCSV:
-        print(row)
-        print(row[0])
-        print(row[0],row[1],row[2],)
+
+        dates = []
+
+        name = row[0]
+        date = row[1]
+        salary = row[2]
+        sick = row[3]
+
+        dates.append(date)
+        print(name + " " + salary)
+
+url-request.get_url("127.0.0.1")
+
+#x = Person("John", "Doe")
+#x.printname()
+#x = Student("Mike", "Olsen",2019)
+#x.welcome()
---
+++ url-request.py
@@ -0,0 +1,7 @@
+import request
+
+def get_url(ip_address):
+    print(ip_address)
+
+def post_url():
+    print("test_post")

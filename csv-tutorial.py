ccffcbe77535bc75fcb9165ec3922a7c37c2fe68
ktamas8 <thomas.kapolnasi@gmail.com>
2019-10-24 14:26:19 +0200

    write post_url def

--- csv-tutorial.py
+++ csv-tutorial.py
@@ -21,9 +21,11 @@ with open('hrdata.csv') as csvfile:
         dates.append(date)
         print(name + " " + salary)
 
-url_request.get_url("http://localhost:5000/device/floor-lamp")
+post_data="{ "identifier": "floor-lamp4", "name": "Floor Lamp5", "device_type": "switch", "controller_gateway": "192.168.0.5" }"
 
-#
+url_request.get_url("http://localhost:5000/device/floor-lamp")
+url_request.put_url("http://localhost:5000/device/floor-lamp", post_data)
+
 #x = Person("John", "Doe")
 #x.printname()
 #x = Student("Mike", "Olsen",2019)
--- url_request.py
+++ url_request.py
@@ -9,5 +9,9 @@ def get_url(api_url):
     gw = data['data']['controller_gateway']
     print(gw)
 
-def post_url():
-    print("test_post")
+def post_url(api_url, data):
+    r = requests.post(url = api_url, data = data)
+    pastebin_url = r.text
+    print("The Pastebin URL is:%s"%pastebin_url)
+
+curl -H "Content-type: application/json" -X POST http://localhost:5000/devices -d ''
